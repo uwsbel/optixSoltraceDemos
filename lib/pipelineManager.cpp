2bd3675ed27984e1109313ed1bd84df6b10dae66
LuningBakke <lfang9@wisc.edu>
2025-03-12 00:05:54 -0500

    More clean up and abstraction for pipeline manager

--- lib/pipelineManager.cpp
+++ lib/pipelineManager.cpp
@@ -3,19 +3,19 @@
 #include <cuda_runtime.h>
 #include <optix.h>
 #include <sutil/vec_math.h>
-#include <lib/geometry.h>
 #include <SoltraceState.h>
 #include <sampleConfig.h>
-#include <optix.h>
 #include <optix_stack_size.h>
 #include <optix_stubs.h>
 // Sutil headers.
 #include <sutil/Exception.h>
 #include <sutil/Record.h>
 #include <sutil/sutil.h>
-#include <sutil/vec_math.h>
+
 #include <cuda/Soltrace.h>
 #include <lib/dataManager.h>
+
+
 #include <lib/pipelineManager.h>
 #include <fstream>
 
@@ -190,28 +190,28 @@ void pipelineManager::createHitGroupProgram(OptixProgramGroup& group,
 // TODO: simplify 
 void pipelineManager::createSunProgram()
 {
-    OptixProgramGroup           sun_prog_group;                 // Handle for the sun program group.
-    OptixProgramGroupOptions    sun_prog_group_options = {};    // Options for the program group (none needed in this case).
-    OptixProgramGroupDesc       sun_prog_group_desc = {};       // Descriptor to define the program group.
+    OptixProgramGroup           group;                 // Handle for the sun program group.
+    OptixProgramGroupOptions    options = {};    // Options for the program group (none needed in this case).
+    OptixProgramGroupDesc       desc = {};       // Descriptor to define the program group.
 
     // Specify the kind of program group (Ray Generation).
-    sun_prog_group_desc.kind = OPTIX_PROGRAM_GROUP_KIND_RAYGEN;
+    desc.kind = OPTIX_PROGRAM_GROUP_KIND_RAYGEN;
     // Link the ray generation program to the sun module and specify the function name.
-    sun_prog_group_desc.raygen.module = m_state.sun_module;
-    sun_prog_group_desc.raygen.entryFunctionName = "__raygen__sun_source";
+    desc.raygen.module = m_state.sun_module;
+    desc.raygen.entryFunctionName = "__raygen__sun_source";
 
     // Create the program group
     OPTIX_CHECK_LOG(optixProgramGroupCreate(
         m_state.context,                 // OptiX context.
-        &sun_prog_group_desc,          // Descriptor defining the program group.
+        &desc,          // Descriptor defining the program group.
         1,                             // Number of program groups to create (1 in this case).
-        &sun_prog_group_options,       // Options for the program group.
+        &options,       // Options for the program group.
         LOG, &LOG_SIZE,                // Logs to capture diagnostic information.
-        &sun_prog_group                // Output: Handle for the created program group.
+        &group                // Output: Handle for the created program group.
     ));
 
-    m_program_groups.push_back(sun_prog_group);
-    m_state.raygen_prog_group = sun_prog_group;
+    m_program_groups.push_back(group);
+    m_state.raygen_prog_group = group;
 }
 
 // Create program group for handling rays interacting with mirrors.
@@ -227,63 +227,43 @@ void pipelineManager::createMirrorProgram()
     m_state.radiance_mirror_prog_group = group;
 }
 
-// Create program group for handling rays interacting with the receiver.
+
+// TODO: think about if we actually need the receiver program ... 
+// since the only difference is the closest hit program, which can be modified 
+// in reflectivity and absorption? 
 void pipelineManager::createReceiverProgram()
 {
+    OptixProgramGroup           group;
+	createHitGroupProgram(group,
+		m_state.geometry_module, "__intersection__parallelogram",
+		m_state.shading_module, "__closesthit__receiver");
 
-
-
-    OptixProgramGroup           radiance_receiver_prog_group;               // Handle for the receiver program group.
-    OptixProgramGroupOptions    radiance_receiver_prog_group_options = {};  // Options for the program group (none needed).
-    OptixProgramGroupDesc       radiance_receiver_prog_group_desc = {};     // Descriptor for the program group.
-
-    // Specify the kind of program group (Hit Group for handling intersections and shading).
-    radiance_receiver_prog_group_desc.kind = OPTIX_PROGRAM_GROUP_KIND_HITGROUP;
-    // Link the intersection shader (geometry handling) to the geometry module.
-    radiance_receiver_prog_group_desc.hitgroup.moduleIS = m_state.geometry_module;
-    radiance_receiver_prog_group_desc.hitgroup.entryFunctionNameIS = "__intersection__parallelogram";
-    // Link the closest-hit shader (shading logic) to the shading module.
-    radiance_receiver_prog_group_desc.hitgroup.moduleCH = m_state.shading_module;
-    radiance_receiver_prog_group_desc.hitgroup.entryFunctionNameCH = "__closesthit__receiver";
-    // No any-hit shader is used in this configuration (set to nullptr).
-    radiance_receiver_prog_group_desc.hitgroup.moduleAH = nullptr;
-    radiance_receiver_prog_group_desc.hitgroup.entryFunctionNameAH = nullptr;
-
-    // Create the program group
-    OPTIX_CHECK_LOG(optixProgramGroupCreate(
-        m_state.context,
-        &radiance_receiver_prog_group_desc,
-        1,
-        &radiance_receiver_prog_group_options,
-        LOG, &LOG_SIZE,
-        &radiance_receiver_prog_group));
-
-    m_program_groups.push_back(radiance_receiver_prog_group);
-    m_state.radiance_receiver_prog_group = radiance_receiver_prog_group;
+    m_program_groups.push_back(group);
+    m_state.radiance_receiver_prog_group = group;
 }
 
 // Create program group for handling rays that miss all geometry.
 void pipelineManager::createMissProgram()
 {
-    OptixProgramGroup           radiance_miss_prog_group;       // Handle for the miss program group.
-    OptixProgramGroupOptions    miss_prog_group_options = {};   // Options for the program group (none needed).
-    OptixProgramGroupDesc       miss_prog_group_desc = {};      // Descriptor for the program group.
+    OptixProgramGroup           group;       // Handle for the miss program group.
+    OptixProgramGroupOptions    options = {};   // Options for the program group (none needed).
+    OptixProgramGroupDesc       desc = {};      // Descriptor for the program group.
 
     // Specify the kind of program group (Miss Program for handling missed rays).
-    miss_prog_group_desc.kind = OPTIX_PROGRAM_GROUP_KIND_MISS;
+    desc.kind = OPTIX_PROGRAM_GROUP_KIND_MISS;
     // Link the miss shader (background or environment shading) to the shading module.
-    miss_prog_group_desc.miss.module = m_state.shading_module;
-    miss_prog_group_desc.miss.entryFunctionName = "__miss__ms";
+    desc.miss.module = m_state.shading_module;
+    desc.miss.entryFunctionName = "__miss__ms";
 
     // Create the program grou
     OPTIX_CHECK_LOG(optixProgramGroupCreate(
         m_state.context,
-        &miss_prog_group_desc,
+        &desc,
         1,
-        &miss_prog_group_options,
+        &options,
         LOG, &LOG_SIZE,
-        &radiance_miss_prog_group));
+        &group));
 
-    m_program_groups.push_back(radiance_miss_prog_group);
-    m_state.radiance_miss_prog_group = radiance_miss_prog_group;
+    m_program_groups.push_back(group);
+    m_state.radiance_miss_prog_group = group;
 }
